eecc032a4d3a2c97f000f0f3d95683f3d8f02e4c
yuji KUDO <yuji.kudo@livesense.co.jp>
2020-12-24 22:40:16 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-console.log(window.document.cookie)
+console.log("cookie-test-window.document.cookie:" + window.document.cookie)
 var cookieString = window.document.cookie
 var cookieArray = cookieString.split(/\s*;\s*/);
 var cookieObject = {};
@@ -6,4 +6,4 @@ for (var i = 0; i < cookieArray.length; i++) {
   var cookiePair = cookieArray[i].split('=');
   cookieObject[cookiePair[0]] = decodeURIComponent(cookiePair[1]);
 }
-console.log(cookieObject)
+console.log("cookieObject" + cookieObject)
